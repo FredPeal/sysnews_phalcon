2b8fb0056bb9f888d57acd056abc13ad94c299b6
FredPeal <frederickpeal@gmail.com>
2019-01-22 13:45:43 -0400

    Que los test generen sus propias pruebas

--- tests/api/IndexNoticiasCept.php
+++ tests/api/IndexNoticiasCept.php
@@ -1,5 +1,7 @@
 <?php 
 
-include 'Test.php';
+include_once 'Test.php';
+$I = new ApiTester($scenario);
+$I->amBearerAuthenticated(generarToken());
 $I->sendGet('/noticias?count=1&page=1');
 $I->seeResponseMatchesJsonType(['titulo' => 'string']);
--- tests/api/Test.php
+++ tests/api/Test.php
@@ -1,6 +1,18 @@
 <?php
+    function generarToken()
+    {
+        $client = new \GuzzleHttp\Client(['base_uri' => 'http://localhost:8080', ]);
+        $response = $client->post(
+            '/auth/',
+            [
+                                                                'form_params' => [
+                                                                    'email' => 'Cantemir@contoso.com',
+                                                                    'pass' => 'Pop'
+                                                                ]
+                                                             ]
+    );
 
-
-    $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDgyNTczMjAsImlzcyI6InN5c25ld3MubG9jYWwiLCJzdWIiOiI0NyIsImVtYWlsIjoiQ2FudGVtaXJAY29udG9zby5jb20iLCJ1c2VybmFtZSI6IkNhbnRlbWlyIiwiaWF0IjoxNTQ4MTcwOTIwfQ.hn7mJujbXCjWzpShxaa0mfRi7U-52hwBqbEAluQFHW4';
-    $I = new ApiTester($scenario);
-    $I->amBearerAuthenticated($token);
+        $body = $response->getBody();
+        $body = json_decode((string) $body);
+        return $body;
+    }
