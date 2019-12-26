65401c51ca1110456ac60d57bad8ebd0b61c2a38
FredPeal <frederickpeal@gmail.com>
2019-12-26 17:31:07 -0400

    Fix images

--- docker/services.sh
+++ docker/services.sh
@@ -1,6 +1,9 @@
 service mysql start &&  \
 /etc/init.d/php7.2-fpm start && \
 service nginx start && \
-mysql -u root --execute="CREATE DATABASE sysnews;" && \
-mysql -u root --database="sysnews" -p < "/app/docker/user.sql" 
+mysql -u root -psecret --execute="CREATE DATABASE sysnews;" && \
+mysql -u root -psecret --execute="CREATE USER 'sysnews' IDENTIFIED BY 'nosenose';" && \
+mysql -u root -psecret --execute="GRANT USAGE ON *.* TO 'sysnews'@localhost IDENTIFIED BY 'nosenose';" && \
+mysql -u root -psecret --execute="GRANT USAGE ON *.* TO 'sysnews'@'%' IDENTIFIED BY 'nosenose';" 
+mysql -u root -psecret --execute="GRANT ALL privileges ON `sysnews`.* TO 'sysnews'@localhost;" && \
 bash
