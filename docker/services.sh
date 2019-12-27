95bf6fe75ac6df92a03c86a09c375a8aefb35361
FredPeal <frederickpeal@gmail.com>
2019-12-27 15:47:25 -0400

    add supervisor to image

--- docker/services.sh
+++ docker/services.sh
@@ -1,9 +1,10 @@
 service mysql start &&  \
 /etc/init.d/php7.2-fpm start && \
 service nginx start && \
-mysql -u root -psecret --execute="CREATE DATABASE sysnews;" && \
-mysql -u root -psecret --execute="CREATE USER 'sysnews' IDENTIFIED BY 'nosenose';" && \
-mysql -u root -psecret --execute="GRANT USAGE ON *.* TO 'sysnews'@localhost IDENTIFIED BY 'nosenose';" && \
-mysql -u root -psecret --execute="GRANT USAGE ON *.* TO 'sysnews'@'%' IDENTIFIED BY 'nosenose';" 
-mysql -u root -psecret --execute="GRANT ALL privileges ON `sysnews`.* TO 'sysnews'@localhost;" && \
-bash
+mysql -u root -psecret --execute="
+CREATE DATABASE sysnews;
+CREATE USER 'sysnews' IDENTIFIED BY 'nosenose';
+GRANT USAGE ON *.* TO 'sysnews'@localhost IDENTIFIED BY 'nosenose';
+GRANT USAGE ON *.* TO 'sysnews'@'%' IDENTIFIED BY 'nosenose';
+GRANT ALL privileges ON sysnews.* TO 'sysnews'@localhost;
+ "
